d0c16c8c0967431da8890c5282b3acfe5a6356d7
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-02-07 20:10:39 +0900

    Added files to display the Utilization "Main screen" (Work-in-progress)

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -1,19 +1,65 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+from flask import Blueprint
+from ckan.config.routing import SubMapper
 
 from ckanext.feedback.command import feedback
 
-class FeedbackPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IClick)
+# Render HTML pages
+# utilization/details.html
+def details():
+    return tk.render('utilization/details.html')
 
-    # IConfigurer
+# utilization/registration.html
+def registration():
+    return tk.render('utilization/registration.html')
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'feedback')
+# utilization/review_approval.html
+def review_approval():
+    return tk.render('utilization/review_approval.html')
+
+# utilization/review.html
+def review():
+    return tk.render('utilization/review.html')
+
+# utilization/search.html
+def search():
+    return tk.render('utilization/search.html')
+
+class FeedbackPlugin(p.SingletonPlugin):
+    # Declare that this class implements IConfigurer
+    p.implements(p.IConfigurer)
+    p.implements(p.IClick)
+    p.implements(p.IBlueprint)
+
+    def update_config(self, config):
+        
+        # Retrieve the value for the "ckan.feedback.substitute_templates" setting from the Config file (/etc/ckan/production.ini) and return it as a bool
+        # If the "ckan.feedback.substitute_templates" setting doesn't exist return False
+        substitute_templates = tk.asbool(config.get('ckan.feedback.substitute_templates', False))
+
+        # If substitute_templates is True, add the feedback directories below to CKAN's extra paths
+        if substitute_templates:
+            # Add this plugin's directories to CKAN's extra paths, so that CKAN will use this plugin's custom files.
+            # Paths are relative to this plugin.py file.
+            tk.add_template_directory(config, 'templates')
+            tk.add_public_directory(config, 'public')
+            tk.add_resource('assets', 'feedback')
+
+    # Return a flask Blueprint object to be registered by the extension
+    def get_blueprint(self):
+        blueprint = Blueprint('search', self.__module__)
+        # Add target page URLs to rules and add each URL to the blueprint
+        rules = [
+            ('/utilization/details', 'details', details),
+            ('/utilization/registration', 'registration', registration),
+            ('/utilization/review_approval', 'review_approval', review_approval),
+            ('/utilization/review', 'review', review),
+            ('/utilization/search', 'search', search), ]
+        for rule in rules:
+            blueprint.add_url_rule(*rule)
+
+        return blueprint
 
     def get_commands(self):
         return [feedback.feedback]
