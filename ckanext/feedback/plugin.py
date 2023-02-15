230e47b0420449e88bc6b379e24785872287c725
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-02-15 16:15:55 +0900

    Added another blank line before class declaration for flake8

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -10,22 +10,6 @@ from ckanext.feedback.services.download import summary as summary_service
 from ckanext.feedback.views import download, utilization
 
 
-# Render HTML pages
-# utilization/details.html
-def details():
-    return tk.render('utilization/details.html')
-# utilization/registration.html
-def registration():
-    return tk.render('utilization/registration.html')
-# utilization/comment_approval.html
-def comment_approval():
-    return tk.render('utilization/comment_approval.html')
-# utilization/recommentview.html
-def comment():
-    return tk.render('utilization/comment.html')
-# utilization/search.html
-def search():
-    return tk.render('utilization/search.html')
 
 class FeedbackPlugin(p.SingletonPlugin):
     # Declare class implements
