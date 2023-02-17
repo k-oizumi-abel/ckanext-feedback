8188dbadfca94135b6bc8a9507268381d6bdd3f9
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-17 11:01:41 +0900

    delete blueprint

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -2,9 +2,7 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 from ckan.common import config
-from flask import Blueprint  # type: ignore
 
-import ckanext.feedback.controllers.download as download
 import ckanext.feedback.services.download.summary as summaryService  # type: ignore
 
 from ckanext.feedback.command import feedback
@@ -19,17 +17,6 @@ class FeedbackPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'feedback')
-
-    def get_blueprint(self):
-        blueprint = Blueprint("download", self.__module__)
-        rules = [
-            ('/increase/<target_resource_id>', 'increase_resource_download_count', download.DownloadController.increase_resource_download_count),
-            ('/download/test', 'test', download.DownloadController.test),
-        ]
-        for rule in rules:
-            blueprint.add_url_rule(*rule)
-
-        return blueprint
 
     def get_commands(self):
         return [feedback.feedback]
@@ -46,6 +33,5 @@ class FeedbackPlugin(plugins.SingletonPlugin):
         return {
             "show_package_download": FeedbackPlugin.show_package_download,
             "show_resource_download": FeedbackPlugin.show_resource_download,
-            "get_resource_download_count": summaryService.get_resource_download_count,
-            "increase_resource_download_count": summaryService.increase_resource_download_count
+            "get_resource_download_count": summaryService.get_resource_download_count
         }
