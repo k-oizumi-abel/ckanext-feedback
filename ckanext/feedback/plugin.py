f4f9a93060d53aa4b66ca4274b45710cc80c390c
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-17 12:04:20 +0900

    add monkey patch about download function

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -2,10 +2,11 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 from ckan.common import config
-from flask import Blueprint  # type: ignore
+from flask import Blueprint
 
-import ckanext.feedback.controllers.download as download
-import ckanext.feedback.services.download.summary as summaryService  # type: ignore
+import ckanext.feedback.services.download.summary as summaryService
+from ckan.ckan.views.resource import download as alias
+from ckan.ckan.views import resource
 
 from ckanext.feedback.command import feedback
 
@@ -19,6 +20,12 @@ class FeedbackPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'feedback')
+
+        def custom_download(package_type, id, resource_id, filename=None):
+            summaryService.increase_resource_download_count(resource_id)
+            alias(package_type, id, resource_id, filename=None)
+
+        resource.download = custom_download
 
     def get_blueprint(self):
         blueprint = Blueprint("download", self.__module__)
