7b58667cf83de97ceba62ea61703cd5493359748
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-17 10:49:18 +0900

    def increase_resource_download_count function

--- ckanext/feedback/services/download/summary.py
+++ ckanext/feedback/services/download/summary.py
@@ -18,21 +18,7 @@ session = Session()
 #    .scalar()
 #
 #    return package_download_count.download
-#
-#    package_download_count = 0
-#    resource_ids = (
-#        session.query(Resource.resource_id)
-#        .filter_by(Resource.package_id = package_id)
-#        .all()
-#    )
-#    for resource_id in resource_ids:
-#        resource_download_count = (
-#            session.query(DownloadSummary.download)
-#            .filter_by(DownloadSummary.resource_id = resource_id)
-#            .scalar()
-#        )
-#        package_download_count = package_download_count + resource_download_count
-#    return package_download_count
+
 
 
 def get_resource_download_count(target_resource_id):
@@ -55,9 +41,7 @@ def increase_resource_download_count(target_resource_id):
         resource_download_count = resource_download_count + 1
         # TODO: renew updated  
     except NoResultFound:
-        # ckan use uuid4
         download_summary_id = text_type(uuid.uuid4())
-        # ckan use datetime.datetime.now
         session.add(
             DownloadSummary(
                 id=download_summary_id,
@@ -69,6 +53,5 @@ def increase_resource_download_count(target_resource_id):
         )
     except Exception as e:
         tk.error_shout(e)
-        sys.exit(1)
 
     session.commit()
