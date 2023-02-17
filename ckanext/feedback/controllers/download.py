6a5752a57417449d929a4f92018fa3a187e3c237
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-17 11:00:44 +0900

    delete test function this file maybe unnecessary

--- ckanext/feedback/controllers/download.py
+++ ckanext/feedback/controllers/download.py
@@ -1,17 +1,7 @@
 import ckan.plugins.toolkit as tk
+from ckanext.feedback.services.download.summary import increase_resource_download_count 
 
 class DownloadController:
-#    def datasets():
-#        return tk.render("")
-#
-#    def dataset_details():
-#        return tk.render("")
-#
-#    def resource_details():
-#        return tk.render("")
-
-    def test():
-        return tk.render("download/download.html")
 
     def increase_resource_download_count(target_resource_id):
         increase_resource_download_count(target_resource_id)
