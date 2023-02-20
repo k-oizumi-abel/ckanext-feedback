a0ca1b749a2736e7650a37322ec3804d68a55927
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-20 14:19:31 +0900

    run black/pflake8 to services/download/summary.py

--- ckanext/feedback/services/download/summary.py
+++ ckanext/feedback/services/download/summary.py
@@ -4,34 +4,44 @@ import datetime
 from sqlalchemy.orm import Session
 from six import text_type
 from sqlalchemy import func
-from psycopg2.errors import UndefinedTable
 from sqlalchemy.exc import ProgrammingError
+from psycopg2 import errors
 
 from ckanext.feedback.models.download import DownloadSummary
-import ckan.plugins.toolkit as tk
+import ckan.plugins.toolkit as toolkit
 from ckan.model import Resource
 
 session = Session()
 log = logging.getLogger(__name__)
 
+
 def get_package_download_count(target_package_id):
     try:
         package_download_count = (
-        session.query(Resource.package_id, func.sum(DownloadSummary.download).label('package_download'))
-        .join(DownloadSummary, Resource.id == DownloadSummary.resource_id)
-        .group_by(Resource.package_id)
-        .filter(Resource.package_id == target_package_id)
-        .first()
+            session.query(
+                Resource.package_id,
+                func.sum(DownloadSummary.download).label('package_download'),
+            )
+            .join(DownloadSummary, Resource.id == DownloadSummary.resource_id)
+            .group_by(Resource.package_id)
+            .filter(Resource.package_id == target_package_id)
+            .first()
         )
-    except ProgrammingError:
-        log.error('download_summary table does not exit. Please hit "feedback init" command')
-    except Exception as e:
-        tk.error_shout(e)
-
-    if package_download_count is None:
+        if package_download_count is None:
             return 0
 
-    return package_download_count.package_download
+        return package_download_count.package_download
+    except ProgrammingError as e:
+        if isinstance(e.orig, errors.UndefinedTable):
+            log.error(
+                'download_summary table does not exit. Hit "feedback init" command'
+            )
+        else:
+            toolkit.error_shout(e)
+        return
+    except Exception as e:
+        toolkit.error_shout(e)
+        return
 
 
 def get_resource_download_count(target_resource_id):
@@ -41,16 +51,26 @@ def get_resource_download_count(target_resource_id):
             .filter_by(resource_id=target_resource_id)
             .scalar()
         )
-        return resource_download_count    
-    except ProgrammingError:
-        log.error('download_summary table does not exit. Please hit "feedback init" command')
+        return resource_download_count
+    except ProgrammingError as e:
+        if isinstance(e.orig, errors.UndefinedTable):
+            log.error(
+                'download_summary table does not exit. Hit "feedback init" command'
+            )
+        else:
+            toolkit.error_shout(e)
+        return
     except Exception as e:
-        tk.error_shout(e)
+        toolkit.error_shout(e)
 
 
 def increase_resource_download_count(target_resource_id):
     try:
-        resource = session.query(DownloadSummary).filter_by(resource_id=target_resource_id).first()
+        resource = (
+            session.query(DownloadSummary)
+            .filter_by(resource_id=target_resource_id)
+            .first()
+        )
         if resource is None:
             download_summary_id = text_type(uuid.uuid4())
             resource_download_summary = DownloadSummary(
@@ -58,14 +78,20 @@ def increase_resource_download_count(target_resource_id):
                 target_resource_id,
                 1,
                 datetime.datetime.now(),
-                datetime.datetime.now()
-                )
+                datetime.datetime.now(),
+            )
             session.add(resource_download_summary)
         else:
             resource.download = resource.download + 1
             resource.updated = datetime.datetime.now()
-        session.commit()        
-    except ProgrammingError:
-        log.error('download_summary table does not exit. Please hit "feedback init" command')
+        session.commit()
+    except ProgrammingError as e:
+        if isinstance(e.orig, errors.UndefinedTable):
+            log.error(
+                'download_summary table does not exit. Hit "feedback init" command'
+            )
+        else:
+            toolkit.error_shout(e)
+        return
     except Exception as e:
-        tk.error_shout(e)
+        toolkit.error_shout(e)
