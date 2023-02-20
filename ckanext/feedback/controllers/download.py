94d816f0bd3e57bceb371b68c21aef6de26b1025
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-20 13:51:01 +0900

    run black/pflake8 to controllers/download.py

--- ckanext/feedback/controllers/download.py
+++ ckanext/feedback/controllers/download.py
@@ -1,8 +1,6 @@
-import ckan.plugins.toolkit as tk
-from ckanext.feedback.services.download.summary import increase_resource_download_count 
+from ckanext.feedback.services.download.summary import increase_resource_download_count
+
 
 class DownloadController:
-
     def increase_resource_download_count(target_resource_id):
         increase_resource_download_count(target_resource_id)
-        return tk.render("download/download.html")
