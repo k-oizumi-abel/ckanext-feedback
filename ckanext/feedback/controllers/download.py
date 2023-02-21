1c70f5689b630d1581fcb22b01cd11ab1991d6df
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-02-21 13:38:33 +0900

    change the function name from custom_download to download

--- ckanext/feedback/controllers/download.py
+++ ckanext/feedback/controllers/download.py
@@ -6,6 +6,6 @@ class DownloadController:
     def count_resource_downloads(resource_id):
         count_resource_downloads(resource_id)
 
-    def custom_download(package_type, id, resource_id, filename=None):
+    def download(package_type, id, resource_id, filename=None):
         count_resource_downloads(resource_id)
         return download(package_type, id, resource_id, filename=filename)
--- ckanext/feedback/views/download.py
+++ ckanext/feedback/views/download.py
@@ -10,8 +10,8 @@ blueprint = Blueprint(
 
 # Add target page URLs to rules and add each URL to the blueprint
 blueprint.add_url_rule(
-    '/<resource_id>/download/<filename>', view_func=DownloadController.custom_download
+    '/<resource_id>/download/<filename>', view_func=DownloadController.download
 )
 blueprint.add_url_rule(
-    '/<resource_id>/download', view_func=DownloadController.custom_download
+    '/<resource_id>/download', view_func=DownloadController.download
 )
