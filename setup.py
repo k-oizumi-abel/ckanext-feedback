7389cdfa5ecac97981809998d136f5203cc71a85
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-03-06 16:01:30 +0900

    rename Testplugin to Feedbackplugin

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         ),
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
     ],
     # What does your project relate to?
     keywords='''CKAN extension opendata open-data review''',
@@ -73,7 +73,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        feedback=ckanext.feedback.plugin:TestPlugin
+        feedback=ckanext.feedback.plugin:FeedbackPlugin
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
     ''',
