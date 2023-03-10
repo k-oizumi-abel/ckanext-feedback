73c21eba6d783b930b1fdc6d289eb74f233125c5
ryo-ma <saka_ro@yahoo.co.jp>
2023-03-10 10:28:00 +0900

    Refactor the code of search.js

--- ckanext/feedback/assets/js/search.js
+++ ckanext/feedback/assets/js/search.js
@@ -3,20 +3,20 @@ function refreshTable() {
   const isWaiting = document.getElementById('waiting').checked;
   const isApproval = document.getElementById('approval').checked;
   const rows = document.querySelectorAll('#results-table tbody tr')
-  var count = 0;
-  
+  let count = 0;
+
   // Loop through all table rows, and hide those who don't match the search query
   rows.forEach(row => {
     const statusCell = row.getElementsByTagName('td')[4]
     if (statusCell.dataset.waiting && isWaiting) {
       row.style.display = 'table-row';
-      count = count + 1;
+      ++count
     } else if (statusCell.dataset.approval && isApproval) {
       row.style.display = 'table-row';
-      count = count + 1;
+      ++count
     } else {
       row.style.display = 'none';
     }
   })
-  document.getElementById('data-count').innerText = '検索結果：' + count + '件';
+  document.getElementById('data-count').innerText = `検索結果：${count}件`;
 }
