9d9e344fbf6ab10479b20d024605ddfb422facc8
ryo-ma <saka_ro@yahoo.co.jp>
2023-03-24 15:46:31 +0900

    Implement to filter by checkbox

--- ckanext/feedback/assets/js/management_comments.js
+++ ckanext/feedback/assets/js/management_comments.js
@@ -1,22 +1,26 @@
 function refreshTable() {
   // Declare variables
-  const isWaiting = document.getElementById('waiting').checked;
-  const isApproval = document.getElementById('approval').checked;
-  const rows = document.querySelectorAll('#results-table tbody tr')
+  const rows = document.querySelectorAll('#results-table tbody tr');
   let count = 0;
 
   // Loop through all table rows, and hide those who don't match the search query
   rows.forEach(row => {
-    const statusCell = row.getElementsByTagName('td')[4]
-    if (statusCell.dataset.waiting && isWaiting) {
+    if (isVisible(row)) {
       row.style.display = 'table-row';
-      ++count
-    } else if (statusCell.dataset.approval && isApproval) {
-      row.style.display = 'table-row';
-      ++count
+      ++count;
     } else {
       row.style.display = 'none';
     }
-  })
+  });
   document.getElementById('data-count').innerText = `検索結果：${count}件`;
+}
+function isVisible(row){
+  const statusCell = row.getElementsByTagName('td')[7];
+  const isWaiting = document.getElementById('waiting').checked && statusCell.dataset.waiting;
+  const isApproval = document.getElementById('approval').checked && statusCell.dataset.approval;
+  const categoryCell = row.getElementsByTagName('td')[5];
+  const categories = Array.from(document.querySelectorAll('.category-checkbox'));
+  const isMatchedCategory = categories.filter(element => element.checked)
+                                      .some(element => element.getAttribute('name') === categoryCell.getInnerHTML());
+  return (isWaiting || isApproval) && isMatchedCategory;
 }
